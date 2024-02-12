94cf238014e62759b60f1ab9dae201a979e9f97b
Gizelle Ramírez Martínez <81934910+GizelleRM@users.noreply.github.com>
2024-02-12 08:20:55 -0600

    Add files via upload

---
+++ build/keys.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    database: {
+        host: 'ls-358d7e8b845a0fac947617c0b2f99cb2350b9fcf.cvgoks268jks.us-east-2.rds.amazonaws.com',
+        user: 'dbmasteruser',
+        password: 'ramygiz.',
+        database: 'tienda_de_ropa'
+    }
+};
